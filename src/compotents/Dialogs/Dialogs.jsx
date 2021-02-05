36b2308f0ea4f89484175c99c57c7bbf05dff64c
Ali Da <al4icrux@gmail.com>
2021-02-05 15:08:09 +0300

    21 fix: dialog page layout;

--- src/compotents/Dialogs/Dialogs.jsx
+++ src/compotents/Dialogs/Dialogs.jsx
@@ -2,7 +2,23 @@ import React, { Component } from "react";
 import s from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
-  return <div className={s.h1}>Dialogs</div>;
+  return (
+    <div>
+      <div className={s.dialogs}>
+        <div className={s.dialogsItems}>
+          <div className={s.dialog + " " + s.active}>Dimych</div>
+          <div className={s.dialog}>Alex</div>
+          <div className={s.dialog}>Volya</div>
+          <div className={s.dialog}>Honor</div>
+        </div>
+        <div className={s.maseeges}>
+          <div className={s.maseege}> hi</div>
+          <div className={s.maseege}> how are you?</div>
+          <div className={s.maseege}> yo</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
