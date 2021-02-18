24d43e82488481258e8d0fb15948fa8bdcf9c778
Ali Da <al4icrux@gmail.com>
2021-02-18 20:25:40 +0300

    52 refactor: side effect to users page;

--- src/compotents/Users/Users.jsx
+++ src/compotents/Users/Users.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import * as axios from "axios";
 import style from "./Users.module.css";
-// import userPhoto from "/../../1220.jpg";
+import userPhoto from "./../../img/1220.jpg";
 
 const Users = (props) => {
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((response) => { props.setUsers(response.data.items); });
-  }
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then((response) => {
+          props.setUsers(response.data.items);
+        });
+    }
+  };
+
   return (
     <div>
+      <button onClick={ getUsers}> Get users </button>
       {props.users.map((u) => (
         <div key={u.id} className={style.item}>
           <span>
             <div>
               <img
-                src={u.photos.small !== null ? u.photos.small : "/../../1220.jpg"}
+                src={u.photos.small !== null ? u.photos.small : userPhoto}
                 alt="foto"
                 className={style.userPhoto}
               />
