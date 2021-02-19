7253516e1ac65c21d896c20325c4178669917239
Ali Da <al4icrux@gmail.com>
2021-02-19 11:48:07 +0300

    54 refactor: add life cycle method componentDidMount for users page;

--- src/compotents/Users/Users.jsx
+++ src/compotents/Users/Users.jsx
@@ -4,15 +4,13 @@ import style from "./Users.module.css";
 import userPhoto from "./../../img/1220.jpg";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
         this.props.setUsers(response.data.items);
       });
   }
-
   render() {
     return (
       <div>
