63d9fed885b2955dcfaa9fdef355e18be04c09d1
Ali Da <al4icrux@gmail.com>
2021-02-22 16:18:33 +0300

    62 feat: follow-unfollow api;

--- src/compotents/Users/Users.jsx
+++ src/compotents/Users/Users.jsx
@@ -2,23 +2,24 @@ import React from "react";
 import style from "./Users.module.css";
 import userPhoto from "./../../img/1220.jpg";
 import { NavLink } from "react-router-dom";
+import * as axios from "axios";
 
 let Users = (props) => {
-    let pagesCount = props.totalUsersCount / props.pageSize;
-    pagesCount = Math.ceil(pagesCount);
-    let pages = [];
-    for (let i = 1; i <= pagesCount; i++) {
-      pages.push(i);
-    }
+  let pagesCount = props.totalUsersCount / props.pageSize;
+  pagesCount = Math.ceil(pagesCount);
+  let pages = [];
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
   return (
     <div>
       <div>
         {pages.map((p) => {
           return (
             <span
-              className={`${
-                 props.currentPage === p && style.selectedPage
-              } ${style.spanPage} `}
+              className={`${props.currentPage === p && style.selectedPage} ${
+                style.spanPage
+              } `}
               onClick={(e) => props.onPageChanged(p)}
             >
               {p}
@@ -26,34 +27,63 @@ let Users = (props) => {
           );
         })}
       </div>
-      { props.users.map((u) => (
+      {props.users.map((u) => (
         <div key={u.id} className={style.item}>
           <span>
             <div>
               <NavLink to={`/profile/${u.id}`}>
-              <img
-                src={u.photos.small !== null ? u.photos.small : userPhoto}
-                alt="foto"
-                className={style.userPhoto}
-              />
+                <img
+                  src={u.photos.small !== null ? u.photos.small : userPhoto}
+                  alt="foto"
+                  className={style.userPhoto}
+                />
               </NavLink>
             </div>
             <div>
               {u.followed ? (
                 <button
                   onClick={() => {
-                     props.unfollow(u.id);
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0//follow/${u.id}`,
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "2db7cc20-6bac-4680-98a4-5b530f945b7d",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resualtCode === 0) {
+                          props.unfollow(u.id);
+                        }
+                      });
                   }}
                 >
-                  follow
+                  Unfollow
                 </button>
               ) : (
                 <button
                   onClick={() => {
-                     props.follow(u.id);
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0//follow/${u.id}`,
+                        {},
+                        {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "2db7cc20-6bac-4680-98a4-5b530f945b7d",
+                          },
+                        }
+                      )
+                      .then((response) => {
+                        if (response.data.resualtCode === 0) {
+                          props.follow(u.id);
+                        }
+                      });
                   }}
                 >
-                  unfollow
+                  Follow
                 </button>
               )}
             </div>
@@ -64,9 +94,9 @@ let Users = (props) => {
               <div>{u.status}</div>
             </span>
             <span className={style.location}>
-              <div>{"Mogilev"} </div> 
+              <div>{"Mogilev"} </div>
               {/* {"u.location.city"} */}
-              <div> {"Belarus"} </div> 
+              <div> {"Belarus"} </div>
               {/* {"u.location.contry"} */}
             </span>
           </span>
