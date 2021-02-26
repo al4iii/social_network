302c7406275f9c66d7e95d636b7d2b059775dd9d
Ali Da <al4icrux@gmail.com>
2021-02-26 12:27:41 +0300

    72 refactor: ProfileStatus component;

--- src/compotents/Profile/ProfileStatus/ProfileStatus.jsx
+++ src/compotents/Profile/ProfileStatus/ProfileStatus.jsx
@@ -9,31 +9,36 @@ class ProfileStatus extends React.Component {
     this.setState({
       editMofe: true,
     });
-  }
-    deActivateEditMofe = () => {
-      this.setState({
-        editMofe: false,
-      })
-    }
+  };
+  deActivateEditMofe = () => {
+    this.setState({
+      editMofe: false,
+    });
+  };
   render() {
     return (
       <div>
         {!this.state.editMofe && (
           <div>
             Status:
-            <span onDoubleClick={this.activateEditMofe.bind(this)}>
+            <span onDoubleClick={this.activateEditMofe}>
               {this.props.status}
             </span>
           </div>
         )}
         {this.state.editMofe && (
           <div>
-            Enter status<input autoFocus={true} onBlur={this.deActivateEditMofe.bind(this)} value={this.props.status}></input>
+            Enter status
+            <input
+              autoFocus={true}
+              onBlur={this.deActivateEditMofe}
+              value={this.props.status}
+            ></input>
           </div>
         )}
       </div>
     );
   }
 }
-  
+
 export default ProfileStatus;
