bd68a45349d39f71cc8122d05baee05f666e81f1
Ali Da <al4icrux@gmail.com>
2021-02-28 18:40:14 +0300

    75 feat: add redux-form for login page;

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,14 +5,15 @@ import navbarReduce from "./navbar-reducer";
 import usersReducer from "./users-reduser";
 import authReducer from "./auth-reduser";
 import thunkMiddleware from "redux-thunk";
-
+import {reducer as formReducer} from 'redux-form';
 
 let reducers = combineReducers({
   prolifePage: profileReducer,
   dialogsPage: dialogsReducer,
   navbarPage: navbarReduce,
   usersPage: usersReducer,
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
