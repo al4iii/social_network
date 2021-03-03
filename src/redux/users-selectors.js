a1716ae2660e78c46a5acd5bbce9ed629ec19813
Ali Da <al4icrux@gmail.com>
2021-03-03 15:16:41 +0300

    83 refactor: add reselect library.

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from "reselect";
+
 export const getPageSize = (state) => state.usersPage.pageSize;
 export const getTotalUsersCount = (state) => state.usersPage.totalUsersCount;
 export const getCurrentPage = (state) => state.usersPage.currentPage;
@@ -6,5 +8,8 @@ export const getFollowingInProgress = (state) =>
   state.usersPage.followingInProgress;
 export const getToggleFollowingInProgress = (state) =>
   state.usersPage.toggleFollowingInProgress;
-export const getUsers = (state) => state.usersPage.users;
+const getUsersSelector = (state) => state.usersPage.users;
 
+export const getUsers = createSelector(getUsersSelector, (users) => {
+  return users.filter((u) => true);
+});
